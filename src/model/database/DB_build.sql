d392b4865f2a9e147facb1139c73bb3d8b741c8e
Victor Masson <victormasson21@gmail.com>
2019-09-10 14:49:17 +0100

    Write sql build script with placeholder values

---
+++ src/model/database/DB_build.sql
@@ -0,0 +1,93 @@
+BEGIN;
+
+DROP TABLE IF EXISTS plans CASCADE;
+DROP TABLE IF EXISTS junction_plans_recipes CASCADE;
+DROP TABLE IF EXISTS recipes CASCADE;
+DROP TABLE IF EXISTS junction_recipes_ingredients CASCADE;
+DROP TABLE IF EXISTS ingredients CASCADE;
+
+CREATE TABLE plans (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(200) NOT NULL,
+  days INTEGER NOT NULL
+);
+
+CREATE TABLE junction_plans_recipes (
+  id SERIAL PRIMARY KEY,
+  plan_id FOREIGN KEY NOT NULL,
+  recipe_id FOREIGN KEY NOT NULL
+);
+
+CREATE TABLE recipes (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL UNIQUE,
+  instructions TEXT NOT NULL,
+  cooking_time INTEGER,
+  health_score INTEGER
+);
+
+CREATE TABLE junction_recipes_ingredients (
+  id SERIAL PRIMARY KEY,
+  recipe_id FOREIGN KEY NOT NULL,
+  ingredient_id FOREIGN KEY NOT NULL
+);
+
+CREATE TABLE ingredients (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(200) NOT NULL UNIQUE
+);
+
+INSERT INTO plans (name, days) VALUES
+(
+  'some plan name',
+  'days integer'
+),
+  (
+  'some plan name',
+  'days integer'
+);
+
+INSERT INTO junction_plans_recipes (plan_id, recipe_id) VALUES
+(
+  'plan foreign key',
+  'recipe foreign key'
+),
+(
+  'plan foreign key',
+  'recipe foreign key'
+);
+
+INSERT INTO recipes (name, instructions, cooking_time, health_score) VALUES
+(
+  'plan name',
+  'cooking instruction',
+  'cooking time integer',
+  'health score integer'
+),
+(
+  'plan name',
+  'cooking instruction',
+  'cooking time integer',
+  'health score integer'
+);
+
+INSERT INTO junction_recipes_ingredients (recipe_id, ingredient_id) VALUES
+(
+  'recipe foreign key',
+  'ingredient foreign key'
+),
+(
+  'recipe foreign key',
+  'ingredient foreign key'
+);
+
+INSERT INTO ingredients (name) VALUES
+(
+  'ingredient name'
+),
+(
+  'ingredient name'
+);
+
+
+COMMIT;
