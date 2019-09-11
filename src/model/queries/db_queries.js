fd68a8d68bb24bc09bc4066a1f76533cfc4c4ce7
Victor Masson <victormasson21@gmail.com>
2019-09-11 14:42:07 +0100

    Query to get shopping list for specific plan

--- src/model/queries/db_queries.js
+++ src/model/queries/db_queries.js
@@ -9,27 +9,37 @@ const getAllPlans = () => {
 //query for disaplying single plan
 
 const getSinglePlan = planId => {
-  return connection.query("SELECT * FROM plans WHERE id = planId;");
+  return connection.query("SELECT * FROM plans WHERE id = {planId};");
 };
 
 // query for displaying recipe info on meal plans page - query returns all recipes related to the given meal plan
 
 const getRecipes = planId => {
   return connection.query(
-    "SELECT * FROM recipes INNER JOIN junction_plans_recipes ON recipes.id = junction_plans_recipes.recipe_id WHERE plan_id = planId;"
+    "SELECT * FROM recipes INNER JOIN junction_plans_recipes ON recipes.id = junction_plans_recipes.recipe_id WHERE plan_id = {planId};"
   );
 };
 
 //query for displaying recipe info on single recipe page
 
 const getSingleRecipe = recipeId => {
-  return connection.query("SELECT * FROM recipes WHERE recipe_id = recipeId;");
+  return connection.query("SELECT * FROM recipes WHERE recipe_id = {recipeId};");
 };
 
 //query for getting ingredients for individual recipes
 
 const getIngredients = recipeId => {
   return connection.query(
-    "SELECT ingredient_name FROM ingredients INNER JOIN junction_recipes_ingredients ON ingredients.id = junction_recipes_ingredients.ingredient_id WHERE recipe_id = recipeId;"
+    "SELECT ingredient_name FROM ingredients INNER JOIN junction_recipes_ingredients ON ingredients.id = junction_recipes_ingredients.ingredient_id WHERE recipe_id = {recipeId};"
   );
 };
+
+// query for getting shopping list for a specific meal plan
+
+const getShoppingList = planId => {
+  return connection.query(
+    "SELECT ingredient_name FROM ingredients INNER JOIN junction_recipes_ingredients ON ingredients.id = junction_recipes_ingredients.ingredient_id INNER JOIN junction_plans_recipes ON junction_plans_recipes.recipe_id = junction_recipes_ingredients.recipe_id WHERE plan_id = {planId};"
+  );
+};
+
+// 
