096705ed447d25136b4751a95c316498d41f2966
frannyfra <francescadelaurentis1987@gmail.com>
2019-09-11 11:57:58 +0100

    Create mealplan partial

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const home = require('./home');
 const newPlan = require('./newplan');
 const error = require('./error');
+const mealPlans = require('./mealplans');
 
 router.get('/', home.get);
 router.get('/newplan', newPlan.get);
+router.get('/mealplans', mealPlans.get)
 router.use(error.client);
 router.use(error.server);
 
