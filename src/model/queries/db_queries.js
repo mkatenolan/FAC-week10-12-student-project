b3826af4bebc61184112bcf6eeb83e8ca8017dbf
Victor Masson <victormasson21@gmail.com>
2019-09-11 16:00:21 +0100

    Complete all basic queries

--- src/model/queries/db_queries.js
+++ src/model/queries/db_queries.js
@@ -1,64 +1,64 @@
 const connection = require("./../database/db_connection");
 
-//query for displaying all meal plans
+// Query for displaying all meal plans
 
 const getAllPlans = () => {
-  return connection.query("SELECT * FROM plans;");
+  return connection.query(`SELECT * FROM plans;`);
 };
 
-//query for disaplying single plan
+// Query for disaplying single plan
 
 const getSinglePlan = planId => {
-  return connection.query("SELECT * FROM plans WHERE id = {planId};");
+  return connection.query(`SELECT * FROM plans WHERE id = {planId};`);
 };
 
-// query for displaying recipe info on meal plans page - query returns all recipes related to the given meal plan
+// Query for displaying recipe info on meal plans page - query returns all recipes related to the given meal plan
 
 const getRecipes = planId => {
   return connection.query(
-    "SELECT * FROM recipes INNER JOIN junction_plans_recipes ON recipes.id = junction_plans_recipes.recipe_id WHERE plan_id = {planId};"
+    `SELECT * FROM recipes INNER JOIN junction_plans_recipes ON recipes.id = junction_plans_recipes.recipe_id WHERE plan_id = {planId};`
   );
 };
 
-//query for displaying recipe info on single recipe page
+// Query for displaying recipe info on single recipe page
 
 const getSingleRecipe = recipeId => {
-  return connection.query("SELECT * FROM recipes WHERE recipe_id = {recipeId};");
+  return connection.query(`SELECT * FROM recipes WHERE recipe_id = {recipeId};`);
 };
 
-//query for getting ingredients for individual recipes
+// Query for getting ingredients for individual recipes
 
 const getIngredients = recipeId => {
   return connection.query(
-    "SELECT ingredient_name FROM ingredients INNER JOIN junction_recipes_ingredients ON ingredients.id = junction_recipes_ingredients.ingredient_id WHERE recipe_id = {recipeId};"
+    `SELECT ingredient_name FROM ingredients INNER JOIN junction_recipes_ingredients ON ingredients.id = junction_recipes_ingredients.ingredient_id WHERE recipe_id = {recipeId};`
   );
 };
 
-// query for getting shopping list for a specific meal plan
+// Query for getting shopping list for a specific meal plan
 
 const getShoppingList = planId => {
   return connection.query(
-    "SELECT ingredient_name FROM ingredients INNER JOIN junction_recipes_ingredients ON ingredients.id = junction_recipes_ingredients.ingredient_id INNER JOIN junction_plans_recipes ON junction_plans_recipes.recipe_id = junction_recipes_ingredients.recipe_id WHERE plan_id = {planId};"
+    `SELECT ingredient_name FROM ingredients INNER JOIN junction_recipes_ingredients ON ingredients.id = junction_recipes_ingredients.ingredient_id INNER JOIN junction_plans_recipes ON junction_plans_recipes.recipe_id = junction_recipes_ingredients.recipe_id WHERE plan_id = {planId};`
   );
 };
 
-// query for getting all recipes in random order (only five to be displayed initially)
+// Query for getting all recipes in random order (only five to be displayed initially)
 
 const getRandomRecipes => {
   return connection.query(
-    "SELECT recipe_name, cooking_time FROM recipes ORDER BY RANDOM();"
+    `SELECT recipe_name, cooking_time FROM recipes ORDER BY RANDOM();`
   );
 };
 
-// query for getting additional suggestions with shared getIngredients
+// Query for getting additional suggestions with shared getIngredients
 
 const getSimilarRecipes = (firstPick, secondPick) => {
   return connection.query(
-    "SELECT recipe_name, cooking_time, health_score FROM recipes WHERE id IN (SELECT DISTINCT recipe_id FROM junction_recipes_ingredients WHERE ingredient_id IN (SELECT ingredient_id FROM junction_recipes_ingredients WHERE recipe_id = {firstPick} OR recipe_id = {secondPick}));"
+    `SELECT recipe_name, cooking_time, health_score FROM recipes WHERE id IN (SELECT DISTINCT recipe_id FROM junction_recipes_ingredients WHERE ingredient_id IN (SELECT ingredient_id FROM junction_recipes_ingredients WHERE recipe_id = {firstPick} OR recipe_id = {secondPick}));`
   );
 };
 
-// query for adding a new plan - title and days - into 'plans' table
+// Query for adding a new plan - title and days - into 'plans' table
 
 const addNewPlan = (planName, planDays) => {
   return connection.query(
@@ -66,10 +66,10 @@ const addNewPlan = (planName, planDays) => {
   );
 };
 
-// query for adding a recipe to a plan
+// Query for adding a recipe to a plan in junction table
 
 const addRecipeToPlan = (planId, recipeId) => {
   return connection.query(
-    "INSERT INTO junction_plans_recipes (plan_id, recipe_id) VALUES ({planId}, {recipeId})"
+    `INSERT INTO junction_plans_recipes (plan_id, recipe_id) VALUES ({planId}, {recipeId})`
   );
 };
