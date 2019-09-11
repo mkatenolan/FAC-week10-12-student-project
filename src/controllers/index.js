c627b9ec96d336d5a0189fdf0b3ff26b95f13d95
Tony Lomax <antl.lomax@gmail.com>
2019-09-11 15:31:27 +0100

    HBS files Unique meal plan page and endpoint Relates #51

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const home = require('./home');
+const home = require("./home");
 // const newPlan = require('./newplan');
-const error = require('./error');
+const error = require("./error");
 // const mealPlans = require('./mealplans');
 // const additionalChoices = require('./additional-choices');
-const routes = require('./routes.js')
-console.log('this is the routes object',routes);
+const routes = require("./routes.js");
+console.log("this is the routes object", routes);
 
-router.get('/', home.get);
-router.get('/newplan', routes.getNewPlan);
-router.get('/mealplans', routes.getMealPlans);
-router.get('/newplan-additional-choices', routes.getAdditionalChoices);
+router.get("/", home.get);
+router.get("/newplan", routes.getNewPlan);
+router.get("/mealplans", routes.getMealPlans);
+router.get("/newplan-additional-choices", routes.getAdditionalChoices);
+router.get("/unique-meal-plan", routes.uniqueMealPlan);
+
 router.use(error.client);
 router.use(error.server);
 
--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,13 +1,17 @@
-//Routes to render each additional page 
+//Routes to render each additional page
 
 exports.getNewPlan = (req, res) => {
-  res.render('newPlan');
-}
+  res.render("newPlan");
+};
 
 exports.getMealPlans = (req, res) => {
-  res.render('mealplans')
-}
+  res.render("mealplans");
+};
 
 exports.getAdditionalChoices = (req, res) => {
-  res.render('newplan-additional-choices')
-}
+  res.render("newplan-additional-choices");
+};
+
+exports.uniqueMealPlan = (req, res) => {
+  res.render("uniqueMealPlan");
+};
