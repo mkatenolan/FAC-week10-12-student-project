5c6a02cf400b3ac16413ba612f592257bd3b5c87
Tony Lomax <antl.lomax@gmail.com>
2019-09-12 17:49:23 +0100

    Fix favicon heroku error

--- src/app.js
+++ src/app.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const exphbs = require('express-handlebars');
-const helpers = require('./views/helpers/index');
-const controllers = require('./controllers/index');
+const express = require("express");
+const path = require("path");
+//const favicon = require('serve-favicon');
+const exphbs = require("express-handlebars");
+const helpers = require("./views/helpers/index");
+const controllers = require("./controllers/index");
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 app.engine(
-  'hbs',
+  "hbs",
   exphbs({
-    extname: 'hbs',
-    layoutsDir: path.join(__dirname, 'views', 'layouts'),
-    partialsDir: path.join(__dirname, 'views', 'partials'),
-    defaultLayout: 'main',
+    extname: "hbs",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+    partialsDir: path.join(__dirname, "views", "partials"),
+    defaultLayout: "main",
     helpers
   })
 );
 
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(controllers);
 
 module.exports = app;
